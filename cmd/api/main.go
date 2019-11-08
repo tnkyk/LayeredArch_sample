5012ec49ca1db061bcf24b90c9bfc8056d02a1e8
tnkyk327 <yuukiyuuki327@gmail.com>
2019-11-08 14:37:20 +0900

    Halfway create test

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,38 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/julienschmidt/httprouter"
-	handler "github.com/tnkyk/LayeredArch_sample/handler/rest"
-	"github.com/tnkyk/LayeredArch_sample/infra/persistence"
-	"github.com/tnkyk/LayeredArch_sample/usecase"
-)
-
 func main() {
-	todoPersistence := persistence.NewTodoPersistence()
-	todoUseCase := usecase.NewTodoUseCase(todoPersistence)
-	todoHandler := handler.NewTodokHandler(todoUseCase)
 
-	userPersistence := persistence.NewUserPersistence()
-	userUseCase := usecase.NewUserUseCase(userPersistence)
-	userHandler := handler.NewUserHandler(userUseCase)
-
-	//ルーティングの設定
-	router := httprouter.New()
-	router.GET("/api/todos", todoHandler.Index)
-	router.GET("/api/todo", todoHandler.GetOneTodo)
-	router.PUT("/api/todo/upsert", todoHandler.UpsertTodo)
-	router.DELETE("/api/todo/delete", todoHandler.DeleteTodo)
-
-	router.GET("/api/users", userHandler.Index)
-	router.GET("/api/user", todoHandler.GetOneTodo)
-	router.PUT("/api/user/upsert", userHandler.UpsertUser)
-	router.DELETE("/api/user/delete", userHandler.DeleteUser)
 	//サーバー起動
-	port := ":3000" //"3000"だとエラーになる
-	fmt.Println(`Server Start >> http:// localhost:%d`, port)
-	log.Fatal(http.ListenAndServe(port, router))
+	Server(":3000")
 }
---
+++ infra/persistence/todo_test.go
@@ -0,0 +1,87 @@
+package persistence
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/tnkyk/LayeredArch_sample/domain/model"
+)
+
+func TestGetAll(t *testing.T) {
+	tp1, _ := time.Parse("2006年01月02日 15時04分05秒", "2019年11月02日 16時05分00秒")
+	tp2, _ := time.Parse("2006年01月02日 15時04分05秒", "2019年11月02日 15時37分20秒")
+	cases := []struct {
+		Id        string
+		Title     string
+		CreatedAt time.Time
+	}{
+		{"4f214757-1ec1-49d7-8465-cac3212ae169", "example3", tp1},
+		{"e51cd7d4-335d-4aa8-9cd1-85ea46569492", "example2", tp2},
+	}
+
+	req, _ := http.NewRequest("GET", fmt.Sprintf("http://localhost:3000/api/todos"), nil)
+	ctx := req.Context()
+	for _, cs := range cases {
+		todos, err := NewTodoPersistence().GetAll(ctx)
+		t.Log(cs.CreatedAt)
+		if err != nil {
+			t.Fatalf(`want get todos`)
+		}
+		for key, todo := range todos {
+			if model.Todo(cs) == todo {
+				break
+			} else if key < len(todos) {
+				continue
+			} else {
+				t.Fatalf(`wantId:%s,wantTitle:%s,wantCreatedAt:%s
+				but getId:%s,getTitle:%s,getCreatedAt:%s`, cs.Id, cs.Title, cs.CreatedAt, todo.Id, todo.Title, todo.CreatedAt)
+			}
+		}
+	}
+}
+
+func TestGetById(t *testing.T) {
+	tp1, _ := time.Parse("2006年01月02日 15時04分05秒", "2019年11月02日 16時05分00秒")
+	tp2, _ := time.Parse("2006年01月02日 15時04分05秒", "2019年11月02日 15時37分20秒")
+	cases := []struct {
+		Id        string
+		Title     string
+		CreatedAt time.Time
+	}{
+		{"4f214757-1ec1-49d7-8465-cac3212ae169", "example3", tp1},
+		{"e51cd7d4-335d-4aa8-9cd1-85ea46569492", "example2", tp2},
+	}
+
+	req, _ := http.NewRequest("GET", fmt.Sprintf("http://localhost:3000/api/todo"), nil)
+	ctx := req.Context()
+	for _, cs := range cases {
+		todo, err := NewTodoPersistence().GetById(ctx, cs.Id)
+		if err != nil {
+			t.Fatalf(`want get todo`)
+		}
+		if todo.Title != cs.Title {
+			t.Fatalf("wantTitle:%s,getTitle:%s", cs.Title, todo.Title)
+		}
+		if todo.CreatedAt != cs.CreatedAt {
+			t.Fatalf("wantCreatedAt:%s,getCreatedAt:%s", cs.CreatedAt, todo.CreatedAt)
+		}
+	}
+}
+
+func TestUpsertTodo(t *testing.T) {
+	tp1, _ := time.Parse("2006年01月02日 15時04分05秒", "2019年11月02日 16時05分00秒")
+	tp2, _ := time.Parse("2006年01月02日 15時04分05秒", "2019年11月02日 15時37分20秒")
+	cases := []struct {
+		CurrentId        string
+		CurrentTitle     string
+		CurrentCreatedAt time.Time
+	}{
+		{"4f214757-1ec1-49d7-8465-cac3212ae169", "example3", tp1},
+		{"e51cd7d4-335d-4aa8-9cd1-85ea46569492", "example2", tp2},
+	}
+	req, _ := http.NewRequest("PUT", fmt.Sprintf("http://localhost:3000/api/upsert"))
+	ctx := req.Context()
+
+}
